b2c93632a366f8e86b3ef99adea13f75f35d5aca
Sivaraj Ponnusamy <rajsivasj@gmail.com>
2024-07-08 12:20:00 +0530

    changes for email

---
+++ emailconfig.java
@@ -0,0 +1,12 @@
+class Email{
+    public static void main(String args[]){
+        String a = "SivarajPonnusamy";
+        String temp="";
+        for(int i=0;i<a.length();i++){
+            temp=temp+a;
+        }
+        System.out.println(temp);
+
+
+    }
+}
